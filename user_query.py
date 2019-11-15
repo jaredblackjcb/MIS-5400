e9d35eda9a24508ea62baa1bd1bcbd2e25c39539
jaredblack <jcblack33@gmail.com>
2019-11-15 15:03:31 -0700

    Create dataframe and run sentiment analysis

--- user_query.py
+++ user_query.py
@@ -2,16 +2,23 @@ from config import TwitterAuthenticate
 from tweepy import API
 from tweepy import Cursor
 import json
+import re
+import pandas as pd
+import numpy as np
+from textblob import TextBlob
 
 class TwitterClient():
     """
     Class retrieves tweets from a particular user and saves them to a json file
     """
-    def __init__(self, fetched_tweets_filename, twitter_user=None):
+    def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticate().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
-        self.fetched_tweets_filename = fetched_tweets_filename
+        # self.fetched_tweets_filename = fetched_tweets_filename
         self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
 
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
@@ -19,15 +26,47 @@ class TwitterClient():
             tweets.append(tweet)
         return tweets
 
+class TweetAnalyzer():
+    '''
+    Create pd data frame
+    '''
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.id for tweet in tweets], columns=['id'])
+
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['user'] = np.array([tweet.user.name for tweet in tweets])
+        df['text'] = np.array([tweet.text for tweet in tweets])
+        df['char_count'] = np.array([len(tweet.text) for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        return df
+
+    def clean_text(self, text):
+        return ' '.join(re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).split())
+
+    def analyze_sentiment(self, text):
+        analysis = TextBlob(self.clean_text(text))
+        return analysis.sentiment.polarity
+
 if __name__ == "__main__":
     user = "realDonaldTrump"
-    file_path = "data/scratch.json"
+    tweet_analyzer = TweetAnalyzer()
+    twitter_client = TwitterClient()
+    api = twitter_client.get_twitter_client_api()
 
-    client = TwitterClient(file_path, user)
-    data = client.get_user_timeline_tweets(10)
-    print(data)
-    print(type(data))
-    print(type(data[0]))
+    desired_width = 320
+    pd.set_option('display.width', desired_width)
+    np.set_printoptions(linewidth=desired_width)
+    pd.set_option('display.max_columns', 10)
+
+    tweets = api.user_timeline(screen_name=user, count=100)
+
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+    df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(text) for text in df['text']])
+
+    print(df)
+
+
+
 
 
     # Extract _json portion of tweepy Status object
